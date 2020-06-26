338d7dcad9155d95b37dee07034633cd73bdb701
structsize <52183019+structsize@users.noreply.github.com>
2020-06-26 11:25:55 +0900

    [CREATE] First App

---
+++ app/src/main/java/com/example/appproject/FindPassword.java
@@ -0,0 +1,14 @@
+package com.example.appproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_password);
+    }
+}
