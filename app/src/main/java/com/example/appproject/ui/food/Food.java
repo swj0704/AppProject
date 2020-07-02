ee63edb6da67776e284e7028435b8379afb72af4
structsize <52183019+structsize@users.noreply.github.com>
2020-07-02 17:54:44 +0900

    [UPDATE] Create food confirmation

--- app/src/main/java/com/example/appproject/ui/food/Food.java
+++ app/src/main/java/com/example/appproject/ui/food/Food.java
@@ -13,23 +13,46 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import com.example.appproject.R;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import kr.go.neis.api.Menu;
+import kr.go.neis.api.NEISException;
+import kr.go.neis.api.School;
+
 public class Food extends Fragment {
 
-    private FoodViewModel foodViewModel;
+    TextView breakfast, lunch, dinner;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        foodViewModel =
-                ViewModelProviders.of(this).get(FoodViewModel.class);
+    Calendar cal = Calendar.getInstance();
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_food, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        foodViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        cal.setTime(new Date());
+        breakfast = root.findViewById(R.id.breakfast);
+        lunch = root.findViewById(R.id.lunch);
+        dinner = root.findViewById(R.id.dinner);
+
+
+        makeText(2020,7,1);
 
         return root;
+    }
+    public void makeText(final int year, final int month, final int day){
+        new Thread(){
+            @Override
+            public void run() {
+                try {
+                    School school = new School(School.Type.HIGH,School.Region.GWANGJU,"F100000120");
+
+                    List<Menu> menu = school.getMonthlyMenu(2020, 7);
+                    breakfast.setText(menu.get(1).getBreakfast());
+                    lunch.setText(menu.get(1).getLunch());
+                    dinner.setText(menu.get(1).getDinner());
+                } catch (NEISException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
     }
 }
