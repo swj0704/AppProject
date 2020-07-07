09b06db27e2482b5a5bbbc888381072aed0262dd
structsize <52183019+structsize@users.noreply.github.com>
2020-07-07 17:01:59 +0900

    [UPDATE] set notification UI

--- adminapp/src/main/java/com/example/adminapp/WriteNotification.java
+++ adminapp/src/main/java/com/example/adminapp/WriteNotification.java
@@ -4,7 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class WriteNotification extends AppCompatActivity {
+
+    DatabaseReference myRef;
+    FirebaseDatabase database;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
